f8d2d2928ab1dbac1d887ddcd76b7d0b4f79f3d4
Muhammad Aammar Tufail <92122579+AammarTufail@users.noreply.github.com>
2024-01-30 14:57:30 +0100

    Update 00_anvio_installation.sh

--- instructors_only/00_anvio_installation.sh
+++ instructors_only/00_anvio_installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --time=5:00:00
 #SBATCH --job-name=anvio_installation
 #SBATCH --output=anvio_installation.out
